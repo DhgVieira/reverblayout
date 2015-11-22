1bd917654940c67ecad5406a24471f975d446ec5
Felipe Girotti <felipe.girotti@gmail.com>
2015-11-22 17:25:58 -0200

    fix check mobile inside controller Ajax

--- application/modules/default/controllers/AjaxcacheController.php
+++ application/modules/default/controllers/AjaxcacheController.php
@@ -10,8 +10,20 @@ class AjaxcacheController extends Zend_Controller_Action
 
     public function topAction()
     {
-        $isMobile = (bool) $this->_request->getParam('isMobile', false);
+        $isMobile = false;
+        $iphone = (bool) strpos($_SERVER['HTTP_USER_AGENT'], "iPhone");
+        $iPad = (bool) strpos($_SERVER['HTTP_USER_AGENT'], "iPad");
+        $android = (bool) strpos($_SERVER['HTTP_USER_AGENT'], "Android");
+        $palmpre = (bool) strpos($_SERVER['HTTP_USER_AGENT'], "webOS");
+        $berry = (bool) strpos($_SERVER['HTTP_USER_AGENT'], "BlackBerry");
+        $ipod = (bool) strpos($_SERVER['HTTP_USER_AGENT'], "iPod");
 
+        //se for mobile eu vou assinar uma variavel para identificar e remover um menu
+        if ($iphone || $android || $palmpre || $ipod || $iPad || $berry == true) {
+            //se entrar e porque e dispositivo movel e assino ao view como true
+            $isMobile = true;
+
+        }
         if ($isMobile) {
             $this->render('topbarmobile');
         } else {
